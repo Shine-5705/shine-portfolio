b4fff317cd1ddd881ec4d457bed64f17567d9967
Parth Sinha <sinhaparth555@gmail.com>
2025-09-13 11:06:57 +0100

    updated every package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { imageService } from '@unpic/astro/service';
+
+import sitemap from '@astrojs/sitemap';
+
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://www.example.com',
+  image: {
+      service: imageService(),
+  },
+
+  integrations: [sitemap(), robotsTxt()]
+});
