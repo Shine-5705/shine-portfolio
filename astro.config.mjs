bcdb655404a4358b74dca142cb9a51c5a3324688
Parth Sinha <sinhaparth555@gmail.com>
2025-09-13 23:03:52 +0100

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,14 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 import robotsTxt from 'astro-robots-txt';
-import vercelServerless from '@astrojs/vercel';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://shine-portfolio-puce.vercel.app',
   output: 'server',
-  adapter: vercelServerless(),
+  adapter: netlify(),
   integrations: [sitemap(), robotsTxt()],
   build: {
     inlineStylesheets: 'never',
