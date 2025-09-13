0d7149b0577e73b1f01a31a774a7225fa92d3080
Parth Sinha <sinhaparth555@gmail.com>
2025-09-13 22:46:15 +0100

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 import robotsTxt from 'astro-robots-txt';
+import vercelServerless from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://shine-portfolio-puce.vercel.app',
-
+  output: 'server',
+  adapter: vercelServerless(),
   integrations: [sitemap(), robotsTxt()],
   build: {
     inlineStylesheets: 'never',
